1f6dff888a5b829c4eef1352b81f87bfb6d8c4b2
Bhim Singh <160112851+BhimSingh-cloudEr@users.noreply.github.com>
2025-02-23 00:22:33 +0530

    Update Repo
    Second commit

--- installation.sh
+++ installation.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+<<<<<<< HEAD
 
 sudo apt-get update
 sleep 3 
 sudo apt-get install openjdk-8-jdk -y 
 sleep 3 
+=======
+sudo apt-get update \
+sleep 3 \
+sudo apt-get install openjdk-8-jdk -y \
+sleep 3 \
+>>>>>>> 570119c (Update Repo)
 sudo apt-get install tomcat8 -y 
